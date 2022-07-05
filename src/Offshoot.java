b481321d213526c496ec1a95666731d53096dab0
Vladimir <98771978+VladIn4e@users.noreply.github.com>
2022-07-05 12:45:42 +0400

    Add woods class

--- src/Offshoot.java
+++ src/Offshoot.java
@@ -1,13 +1,14 @@
 public class Offshoot {
     int x, y;
-    int[][] gen = new int[15][9];
-    int utk;
+    int[][] gen = new int[15][4];
+    int utk, index;
 
-    public Offshoot(int x, int y, int[][] gen) {
+    public Offshoot(int x, int y, int[][] gen, int utk, int index) {
         this.x = x;
         this.y = y;
         this.gen = gen.clone();
-        this.utk = 0;
+        this.utk = utk;
+        this.index = index;
     }
 
 }
